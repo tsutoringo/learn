2de3192debaffdd114c7a0f25e6b477e2b4741ae
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2024-09-16 04:44:20 +0900

    improve: コメント追加

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 // 三角形の面積を求める
 function triangle(bottom: number, height: number) {
+//                      ^^^^^^^^        ^^^^^^^^
+//                      └───────────────┤
+//                                      引数の後ろに`:`と`型`を書き、`bottom`と`height`が`number`型ということを明示する。
     return bottom * height / 2;
 }
 
 const bottom = prompt("底辺を入力してください。");
 const area = triangle(bottom, 5);
+//                    ^^^^^^
+//                    │
+//                    `bottom`は`string`型で、`triangle`関数の１つ目の引数が`number`型が指定されているため、エラーが発生する。
   
